a40fe6dcd265a65996f32c492f37e69d2744bc66
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-04-04 23:18:45 +0100

    prim tag full

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
   has_many :influencer_tags
-  has_one :influencer
+
 end
--- app/serializers/influencer_serializer.rb
+++ app/serializers/influencer_serializer.rb
@@ -1,4 +1,7 @@
 class InfluencerSerializer < ActiveModel::Serializer
-  attributes :id, :handle, :platform, :followers, :profile_pic_url, :primary_tag_id, :tags
+  attributes :id, :handle, :platform, :followers, :profile_pic_url, :primary_tag, :tags
 
+  def primary_tag
+    Tag.find(object.primary_tag_id)
+  end
 end

85b9b0237ca0cbb12a606658f6fa07a3cd323962
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-04-05 14:38:57 +0100

    validations done

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -3,13 +3,4 @@ class Influencer < ApplicationRecord
   has_many :influencer_tags, dependent: :destroy
   has_many :tags, through: :influencer_tags
 
-  validate :primary_tag_is_not_in_secondary_tags
-
-
-  def primary_tag_is_not_in_secondary_tags
-    if self.influencer_tags.any? { |x| x.tag_id == self.primary_tag_id }
-      errors.add(:primary_tag_id, "cannot be the same as one of the tags")
-    end
-  end
-
 end
--- app/models/influencer_tag.rb
+++ app/models/influencer_tag.rb
@@ -2,4 +2,12 @@ class InfluencerTag < ApplicationRecord
   belongs_to :influencer
   belongs_to :tag
 
+  validate :tag_is_not_the_same_as_primary_tag
+
+
+  def tag_is_not_the_same_as_primary_tag
+    if self.tag_id == self.influencer.primary_tag_id
+      errors.add(:tag_id, "cannot be the same as influencer's primary tag")
+    end
+  end
 end
